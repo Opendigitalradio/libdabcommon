16c31f4673c93bef5f9d35b13accde6e3063abe3
Felix Morgner <felix.morgner@gmail.com>
2017-08-01 12:27:57 +0200

    cmake: better conan integration

--- cmake/modules/ConanPackages.cmake
+++ cmake/modules/ConanPackages.cmake
@@ -1,30 +1,74 @@
-if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/conanfile.txt")
-  message(STATUS "Current conanfile.txt directory: ${CMAKE_CURRENT_SOURCE_DIR}")
-  if(NOT EXISTS "${CMAKE_BINARY_DIR}/conanbuildinfo.cmake")
-    if(${CMAKE_CXX_COMPILER_ID} MATCHES "GNU")
-      set(CONAN_COMPILER_NAME "gcc")
-    elseif(${CMAKE_CXX_COMPILER_ID} MATCHES "Clang")
-      set(CONAN_COMPILER_NAME "clang")
-    else()
-      message(FATAL_ERROR "ConanPackages: Unknown compiler ${CMAKE_CXX_COMPILER_ID}")
-    endif()
+include(CMakeParseArguments)
 
-    message(STATUS "Collecting conan packages")
-    string(SUBSTRING ${CMAKE_CXX_COMPILER_VERSION} 0 3 CONAN_COMPILER_VERSION)
+function(install_conan_packages)
+  set(OPTIONS SYSTEM_HEADERS)
+  set(SV_ARGUMENTS LIBCXX CONANFILE)
+  cmake_parse_arguments(INSTALL_CONAN "${OPTIONS}" "${SV_ARGUMENTS}" "" ${ARGN})
 
-    execute_process(
-      COMMAND
-      conan install --build=missing --generator cmake ${CMAKE_CURRENT_SOURCE_DIR} -s compiler=${CONAN_COMPILER_NAME} -s compiler.version=${CONAN_COMPILER_VERSION} -s compiler.libcxx=libstdc++
-      OUTPUT_QUIET
-      )
+  if(${CMAKE_CXX_COMPILER_ID} MATCHES "GNU")
+    set(_COMPILER "gcc")
+  elseif(${CMAKE_CXX_COMPILER_ID} MATCHES "Clang")
+    set(_COMPILER "clang")
+  else()
+    message(FATAL_ERROR "Conan: Unknown compiler ${CMAKE_CXX_COMPILER_ID}")
   endif()
-else()
-  message(FATAL_ERROR "No conanfile found at '${CMAKE_CURRENT_SOURCE_DIR}/conanfile.txt'")
-endif()
 
-include("${CMAKE_BINARY_DIR}/conanbuildinfo.cmake")
+  string(SUBSTRING ${CMAKE_CXX_COMPILER_VERSION} 0 3 _COMPILER_VERSION)
 
-set(CONAN_SYSTEM_INCLUDES ON)
-conan_check_compiler()
-conan_flags_setup()
-conan_set_find_paths()
+  if(NOT DEFINED INSTALL_CONAN_LIBCXX)
+    if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+      set(_LIBCXX "libc++")
+    elseif(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+      if(${_COMPILER} MATCHES "gcc" AND ${_COMPILER_VERSION} VERSION_LESS "5.1")
+        set(_LIBCXX "libstdc++")
+      else()
+        set(_LIBCXX "libstdc++11")
+      endif()
+    else()
+      message(FATAL_ERROR "Conan: failed to determine C++ standard library")
+    endif()
+  else()
+    set(_LIBCXX ${INSTALL_CONAN_LIBCXX})
+  endif()
+
+  if(NOT DEFINED INSTALL_CONAN_CONANFILE)
+    if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/conanfile.py")
+      set(_CONANFILE "${CMAKE_CURRENT_SOURCE_DIR}/conanfile.py")
+    elseif(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/conanfile.txt")
+      set(_CONANFILE "${CMAKE_CURRENT_SOURCE_DIR}/conanfile.txt")
+    else()
+      message(FATAL_ERROR "Conan: could not find valid conanfile")
+    endif()
+  else()
+    get_filename_component(_CONANFILE "${INSTALL_CONAN_CONANFILE}" REALPATH BASE_DIR "${CMAKE_BINARY_DIR}")
+    if(NOT EXISTS "${_CONANFILE}")
+      message(FATAL_ERROR "Conan: the specified conanfile '${_CONANFILE}' does not exist")
+    endif()
+  endif()
+
+  message(STATUS "Conan: current conanfile: ${_CONANFILE}")
+
+  if(NOT EXISTS "${CMAKE_BINARY_DIR}/conanbuildinfo.cmake")
+    message(STATUS "Conan: installing packages")
+    get_filename_component(_CONANFILE "${_CONANFILE}" DIRECTORY)
+    execute_process(COMMAND conan
+      install
+      --build=missing
+      --generator=cmake
+      -s compiler=${_COMPILER}
+      -s compiler.version=${_COMPILER_VERSION}
+      -s compiler.libcxx=${_LIBCXX}
+      ${_CONANFILE}
+      OUTPUT_FILE "${CMAKE_BINARY_DIR}/${PROJECT_NAME}_conan_install.log"
+      RESULT_VARIABLE _RESULT)
+    if(${_RESULT})
+      message(FATAL_ERROR "Conan: failed to install packages. Check ${CMAKE_BINARY_DIR}/${PROJECT_NAME}_conan_install.log")
+    endif()
+  endif()
+
+  include("${CMAKE_BINARY_DIR}/conanbuildinfo.cmake")
+  if(DEFINED INSTALL_CONAN_SYSTEM_HEADERS)
+    set(CONAN_SYSTEM_INCLUDES ON)
+  endif()
+  conan_basic_setup()
+endfunction()
